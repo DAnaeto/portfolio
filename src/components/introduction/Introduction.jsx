902d20d5aef8499aef593f18169f2a4d5d36f3dd
Dominic Anaeto <dominic.anaeto@gmail.com>
2021-06-06 19:42:58 -0400

    Added intro page design

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import { React, useEffect, useRef } from "react";
+import { init } from "ityped";
 import "./introduction.scss";
 
 export default function Introduction() {
-  return <div className="introduction" id="intro"></div>;
+  const text = useRef();
+
+  useEffect(() => {
+    init(text.current, {
+      showCursor: true,
+      backDelay: 1500,
+      strings: ["Analyst", "Engineer"],
+    });
+  }, []);
+
+  return (
+    <div className="introduction" id="intro">
+      <div className="left">
+        <div className="images">
+          <img src="sources/portrait.png" alt="Portrait of me" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrap">
+          <h2>Hello, I'm</h2>
+          <h1>Dominic Anaeto</h1>
+          <h3>
+            Cybersecurity <span ref={text}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="sources/down.png" alt="Down arrow to go to next section" />
+        </a>
+      </div>
+    </div>
+  );
 }
