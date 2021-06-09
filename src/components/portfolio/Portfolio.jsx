abf15ff00c6dc22cc2b64465ffab7ce571acb915
Dominic Anaeto <dominic.anaeto@gmail.com>
2021-06-09 19:52:26 -0400

    Added template for portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import { React } from 'react'
 import './portfolio.scss'
 
 export default function Portfolio() {
+
+    // let [activeItem, setActiveItem] = useState("first")
     return (
-        <div className="portfolio" id="portfolio">
-            
+      <div className="portfolio" id="portfolio">
+        <h1>Portfolio</h1>
+        <ul>
+          <li className="active">Filler</li>
+          <li>Filler</li>
+          <li>Filler</li>
+          <li id="last">Filler</li>
+        </ul>
+        <div className="container">
+          <div className="item">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU"
+              alt="work"
+            />
+            <h3>App</h3>
+          </div>
+          <div className="item">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU"
+              alt="work"
+            />
+            <h3>App</h3>
+          </div>
+          <div className="item">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU"
+              alt="work"
+            />
+            <h3>App</h3>
+          </div>
+          <div className="item">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU"
+              alt="work"
+            />
+            <h3>App</h3>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
