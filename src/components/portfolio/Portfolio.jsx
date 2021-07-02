6ed1743d3ff455fc5bf3c6af1183be07e6949a6f
Dominic Anaeto <dominic.anaeto@gmail.com>
2021-07-02 01:46:06 -0400

    Made the portfolio page look cleaner, also added data.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,32 +8,50 @@ export default function Portfolio() {
         title: "Education",
         icon: 'sources/mortarboard.png',
         desc: 'University of Connecticut',
-        extra: 'Major: Computer Science'
+        extra: 'Computer Science'
     },
     {
-        id: '2',
-        title: 'Work Experience',
-        icon: 'sources/portfolio.png',
+      id: '2',
+      title: 'Work Experience',
+      icon: 'sources/portfolio.png',
       desc: "Cisco Meraki",
-        extra: 'Software QA Engineer Intern'
-    },
+      extra: 'Software QA Engineer Intern',
+      information: <div className="rightContainer"><p>Created a project using Ruby on Rails</p>
+                    <p>Utilized the Jenkins API to retrieve test information</p>
+                    <p>Formulated solutions to problems using Ruby scripts</p>
+                    <p>Used Jira for bug and progress tracking</p>
+                    <p>Used Git for version control</p>
+                    <p>
+                      Connected an external PostgreSQL database and configured a
+                      MongoDB locally on a VM
+                    </p>
+                    <p>
+                      Incorporated Cron to schedule a script that periodically
+                      updates a local MongoDB instance
+                    </p>
+                    <p>Learned about container technology like Docker</p></div>
+      },
     {
-        id: '3',
-        title: 'Relevant Courses',
-        icon: 'sources/online-course.png'
-    },
-    {
-      id: '4',
-      title: 'Clubs',
-      icon: 'sources/network.png'
-    },
-    {
-      id: '5',
+      id: '3',
       title: 'Technical Skills',
       icon: 'sources/pencil.png'
+      },
+    
+    {
+        id: '4',
+        title: 'Relevant Courses',
+        icon: 'sources/online-course.png'
+      },
+    
+    {
+      id: '5',
+      title: 'Clubs',
+      icon: 'sources/network.png'
     }
+    
     ]
-    const clickHandler = (num) => { //0 is left 1 is right
+  const clickHandler = (num) => {
+      // 0 is the left arrow and 1 is right arrow
         if (num === 0) {
             setCurrentSlide(
               currentSlide > 0 ? currentSlide - 1 : data.length - 1
@@ -54,16 +72,22 @@ export default function Portfolio() {
             <div className="container">
               <div className="item">
                 <div className="left">
-                  <div className="leftContaine">
+                  <div className="leftContainer">
                     <div className="imgContainer">
-                      <img src={item.icon} alt="Icon" style={{ height: `50px` }}/>
+                      <img
+                        src={item.icon}
+                        alt="Icon"
+                        style={{ height: `50px` }}
+                      />
                     </div>
                     <h2>{item.title}</h2>
-                    { item['desc'] ? <p>{item.desc}</p> : <div></div> }
-                    { item['extra'] ? <span>{item.extra}</span> : <div></div> }
+                    {item["desc"] ? <p>{item.desc}</p> : <div></div>}
+                    {item["extra"] ? <span>{item.extra}</span> : <div></div>}
                   </div>
                 </div>
-                <div className="right"></div>
+                <div className="right">  
+                    {item.information}
+                </div>
               </div>
             </div>
           ))}
